45168a60854a054ab7f5eb15b88c3d3415973462
Angelina <loglinn05@gmail.com>
2025-08-27 22:58:40 +0300

    Initial version

---
+++ client-react/vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import dns from 'dns';
+
+dns.setDefaultResultOrder('verbatim');
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true, // helps with Docker/WSL file change events
+    },
+    proxy: {
+      '/api/golang': {
+        target: 'http://api-golang:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/golang/, ''),
+        secure: false,
+      },
+      '/api/node': {
+        target: 'http://api-node:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/node/, ''),
+        secure: false,
+      },
+    },
+  },
+});
